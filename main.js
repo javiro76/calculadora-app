5af8bf9ebdb7d5e8d5df78cb6560770339c9ce1d
javiro76 <javiro76@gmail.com>
2023-03-01 19:36:27 -0500

    se programa app.js para renderizar html

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import './style.css'
 import calculadoraStore from "./src/store/calculadora.store";
+import { App } from './src/calculadora/app';
+
+
 
 calculadoraStore.initStore();
+
+App('#app');
--- src/calculadora/app.js
+++ src/calculadora/app.js
@@ -1,2 +1,13 @@
 import html from "./app.html?raw";
 import calculadoraStore from '../store/calculadora.store';
+
+export const App = ( elementId ) => {
+
+    (()=> {
+        const app = document.createElement('div');
+        app.innerHTML = html;
+        document.querySelector(elementId).append( app );
+    })();
+
+
+}; 
