3be9af8b151a38fc62e149f314e03ef47e4f48be
javiro76 <javiro76@gmail.com>
2023-03-06 20:12:43 -0500

    -se hace funciones de operaciones matematicas

--- src/store/calculadora.store.js
+++ src/store/calculadora.store.js
@@ -7,3 +7,47 @@ const initStore = () => {
 export default {
     initStore,
 };
+
+/**Funcion que realiza operacion de multiplicación
+ * 
+ * @param {Number} num1 
+ * @param {Number} num2 
+ * @returns 
+ */
+const multiplicar = (num1,num2) => {
+
+    if (!num1 || !num2 ) throw new Error ('Los dos números son requeridos')
+
+    return num1*num2
+
+};
+
+const dividir = (num1,num2) => {
+
+    if (!num1 || !num2 ) throw new Error ('Los dos números son requeridos')
+
+    return num1/num2
+
+};
+
+const sumar = (num1,num2) => {
+
+    if (!num1 || !num2 ) throw new Error ('Los dos números son requeridos')
+
+    return num1+num2
+
+};
+
+const restar = (num1,num2) => {
+
+    if (!num1 || !num2 ) throw new Error ('Los dos números son requeridos')
+
+    return num1-num2
+
+};
+
+
+console.log(multiplicar(3,4)); 
+console.log(dividir(4,8));
+console.log(sumar(4,8));
+console.log(restar(4,8));
